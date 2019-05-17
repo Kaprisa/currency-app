0fa45d7a17e28f2cfc2d21ccc1a069a0e78d3fb1
kaprisa57@gmail.com <kaprisa57@gmail.com>
2019-05-17 10:09:07 +0300

    - changed public path, added build

---
+++ build/precache-manifest.a648ef720dff54ae125d6e9866a2327d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86dbc3e02eeb65f99f315cc9aef2ea62",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cd7f8e2dbc2df7f4cf9a",
+    "url": "./static/css/main.cdbbb060.chunk.css"
+  },
+  {
+    "revision": "30daddbfc18ed87133c0",
+    "url": "./static/js/2.76a58a40.chunk.js"
+  },
+  {
+    "revision": "cd7f8e2dbc2df7f4cf9a",
+    "url": "./static/js/main.b0b4268d.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
